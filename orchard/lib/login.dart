dffa99b22ce57ca2dc49eb025195f72d62909aaf
Marco Salazar <marcosalazar16@gmail.com>
2021-11-16 21:11:37 -0600

    added splash and login view

--- orchard/lib/login.dart
+++ orchard/lib/login.dart
@@ -7,11 +7,13 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen[350],
+      appBar: AppBar(
+        title: const Text("My Orchard"),
+      ),
       body: SingleChildScrollView(
         child: Column(
-          children: const <Widget>[
-            Padding(
+          children: <Widget>[
+            const Padding(
                 padding: EdgeInsets.only(top: 60.0),
                 child: Center(
                   child: SizedBox(
@@ -19,22 +21,30 @@ class LoginPage extends StatelessWidget {
                     height: 150,
                   ),
                 )),
-            Padding(
+            const Padding(
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), labelText: 'Email'),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
               child: TextField(
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), labelText: 'Password'),
               ),
-            )
+            ),
+            TextButton(
+                style: TextButton.styleFrom(
+                    primary: Colors.blue[300],
+                    textStyle: TextStyle(fontSize: 20),
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, top: 15.0, bottom: 0)),
+                onPressed: () {},
+                child: Text("Login"))
           ],
         ),
       ),
